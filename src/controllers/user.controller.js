b93e2eec1a31601a0b70aaa40015dfb3652b034b
Krish <krish.khattar@gmail.com>
2025-02-07 16:54:25 +0530

    added all crud operations

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { client } from "../config/database.js";
 
+// Create user
 const registerUser = asyncHandler(async (req, res) => {
   const { name, id } = req.body;
 
@@ -22,4 +23,64 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 });
 
-export default registerUser;
+// Read user
+const readUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  const select_query = `SELECT * FROM demotable where id = $1`;
+  client.query(select_query, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({
+        success: false,
+        message: "Error reading data from database",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        data: result.rows,
+      });
+    }
+  });
+});
+
+// Update user
+const updateUser = asyncHandler(async (req, res) => {
+  const { name, id } = req.body;
+  const update_query = `UPDATE demotable SET name = $1 WHERE id = $2`;
+  client.query(update_query, [name, id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({
+        success: false,
+        message: "Error updating data in database",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Data updated successfully",
+      });
+    }
+  });
+});
+
+// Delete user
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  const delete_query = `DELETE FROM demotable WHERE id = $1`;
+  client.query(delete_query, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({
+        success: false,
+        message: "Error deleting data from database",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Data deleted successfully",
+      });
+    }
+  });
+});
+
+export { registerUser, readUser, updateUser, deleteUser };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import registerUser from "../controllers/user.controller.js";
+import {
+  registerUser,
+  readUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.route("/post").post(registerUser);
+router.route("/registerUser").post(registerUser);
+router.route("/readUser").post(readUser);
+router.route("/updateUser").post(updateUser);
+router.route("/deleteUser").post(deleteUser);
 
 export default router;
